009b4e4c75d90e5965f7e63228de4976743de3eb
Baris <Krutciatus@gmail.com>
2019-05-11 20:39:48 +0700

    финальный правки

--- source/js/script.js
+++ source/js/script.js
@@ -9,15 +9,18 @@ var surname = document.querySelector("[name=surname]");
 var popupOK = document.querySelector(".popup__button--ok");
 var popupClose = document.querySelector(".popup__button--close");
 var submit = document.querySelector(".form__submit");
+var header = document.querySelector(".page-header");
 
 navMain.classList.remove("main-nav--nojs");
 navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("main-nav--closed")) {
     navMain.classList.remove("main-nav--closed");
     navMain.classList.add("main-nav--opened");
+    header.classList.add("page-header--menu-opened");
   } else {
     navMain.classList.add("main-nav--closed");
     navMain.classList.remove('main-nav--opened');
+    header.classList.remove("page-header--menu-opened");
   }
 });
 
